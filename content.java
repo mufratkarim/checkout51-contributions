12752680e13c8d6c7827566c095a16320731a93d
Mufrat Aritra <mufratkarimaritra@gmail.com>
2023-12-11 11:48:58 -0500

    fix/C51SMRT-4526 Add params to single offer BatchApi call (#2127)
    * fix/C51SMRT-4526 Add params to single offer BatchApi call
    
    * add params to all endpoints, remove unused/unneeded code

---
+++ content.java
@@ -0,0 +1,16 @@
+public class Cuxdfl {
+    public static void main() {
+        System.out.println("fufrt");
+        System.out.println("pdghm");
+        System.out.println("pxrua");
+        System.out.println("hqypf");
+        System.out.println("tuwlb");
+        System.out.println("qoeya");
+        System.out.println("eniim");
+        System.out.println("meuvf");
+        System.out.println("swcit");
+        System.out.println("mptjr");
+        System.out.println("nrtfv");
+        System.out.println("fuqwx");
+    }
+}
