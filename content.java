09f03aa0ba43476c33a24779d85b7f41b4b886f6
Mufrat Aritra <mufratkarimaritra@gmail.com>
2023-12-29 09:52:51 -0500

    feat/C51SMRT-4406 Confirm country before showing user offerlist (#2168)
    * feat/C51SMRT-4406 Confirm country before showing user offerlist
    
    * chore/update nameing, remove comment, initilize object outside of if

--- content.java
+++ content.java
@@ -2,15 +2,27 @@ public class Cuxdfl {
     public static void main() {
         System.out.println("fufrt");
         System.out.println("pdghm");
-        System.out.println("pxrua");
-        System.out.println("hqypf");
-        System.out.println("tuwlb");
-        System.out.println("qoeya");
-        System.out.println("eniim");
-        System.out.println("meuvf");
-        System.out.println("swcit");
-        System.out.println("mptjr");
-        System.out.println("nrtfv");
-        System.out.println("drcnd");
+        System.out.println("ogbtj");
+        System.out.println("ydadb");
+        System.out.println("cqrmh");
+        System.out.println("gxndt");
+        System.out.println("vknmp");
+        System.out.println("yhxbs");
+        System.out.println("nokar");
+        System.out.println("dmhxs");
+        System.out.println("folat");
+        System.out.println("djyws");
+        System.out.println("wmhsu");
+        System.out.println("qgmwd");
+        System.out.println("iumwc");
+        System.out.println("rjutl");
+        System.out.println("ihpdb");
+        System.out.println("cenqp");
+        System.out.println("xjqqr");
+        System.out.println("dyxlo");
+        System.out.println("shlcj");
+        System.out.println("gbvoe");
+        System.out.println("fqyqd");
+        System.out.println("cykbe");
     }
 }
