5b2db3a677e07634d379ef9e7e45c59688b68c1f
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-01-10 11:37:27 -0500

    Revert "feat/C51SMRT-4406 Confirm country before showing user offerlist" (#2192)
    Revert "feat/C51SMRT-4406 Confirm country before showing user offerlist (#2168)"
    
    This reverts commit db71d5eef21fb2b250d4c3a6b866249acd4a9961.

--- content.java
+++ content.java
@@ -2,27 +2,15 @@ public class Cuxdfl {
     public static void main() {
         System.out.println("fufrt");
         System.out.println("pdghm");
-        System.out.println("ogbtj");
-        System.out.println("ydadb");
-        System.out.println("cqrmh");
-        System.out.println("gxndt");
-        System.out.println("vknmp");
-        System.out.println("yhxbs");
-        System.out.println("nokar");
-        System.out.println("dmhxs");
-        System.out.println("folat");
-        System.out.println("djyws");
-        System.out.println("wmhsu");
-        System.out.println("qgmwd");
-        System.out.println("iumwc");
-        System.out.println("rjutl");
-        System.out.println("ihpdb");
-        System.out.println("cenqp");
-        System.out.println("xjqqr");
-        System.out.println("dyxlo");
-        System.out.println("shlcj");
-        System.out.println("gbvoe");
-        System.out.println("fqyqd");
-        System.out.println("cykbe");
+        System.out.println("vunov");
+        System.out.println("yqqds");
+        System.out.println("bktgo");
+        System.out.println("phqdi");
+        System.out.println("uxjgj");
+        System.out.println("prfvn");
+        System.out.println("lmpwt");
+        System.out.println("sbllk");
+        System.out.println("paeqw");
+        System.out.println("wempq");
     }
 }
