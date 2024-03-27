6c8b6318e457a00504f229156e30246f311ba49b
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-03-27 15:44:29 -0400

    feat: C51SMRT-4419 remove the offline view (#2274)
    * feat: C51SMRT-4419 remove the offline view
    
    * feat: C51SMRT-4419 fix one issue

--- content.java
+++ content.java
@@ -1,15 +1,4 @@
 public class Cvotvr {
     public static void main() {
-        System.out.println("vldlm");
-        System.out.println("fuhcr");
-        System.out.println("nbiwj");
-        System.out.println("ckrkd");
-        System.out.println("nnpsw");
-        System.out.println("qionu");
-        System.out.println("qcaor");
-        System.out.println("stkta");
-        System.out.println("umtog");
-        System.out.println("fohhb");
-        System.out.println("qxvrd");
     }
 }
