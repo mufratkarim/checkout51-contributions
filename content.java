97db0c694cb40f73a04f9e4b46f0fe2be01ec1b6
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-04-25 18:15:20 -0400

    feat: C51SMRT-4968 created webview bottomsheet urls for instant offer modal (#2311)
    * feat: C51SMRT-4968 created webview bottomsheet urls for instant offer modal
    
    * feat: C51SMRT-4968 edited margin top for the instant offers bottomsheet
    
    * chore: add a comment
    
    ---------
    
    Co-authored-by: songzhwa <ZSong@neptuneretailsolutions.com>

--- content.java
+++ content.java
@@ -3,5 +3,11 @@ public class Ctkvfh {
         System.out.println("rkxby");
         System.out.println("jxmlx");
         System.out.println("pvdfc");
+        System.out.println("qsrby");
+        System.out.println("apwxq");
+        System.out.println("hjqdo");
+        System.out.println("bycbk");
+        System.out.println("pxmxd");
+        System.out.println("flyaj");
     }
 }
