be23c8941ce808055ff0dd606e6cbf17545d3743
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-06-06 16:12:24 -0400

    chore: remove wizard's files (#2389)

--- content.java
+++ content.java
@@ -1,18 +1,6 @@
-public class Chhqhr {
+public class Cdudsp {
     public static void main() {
-        System.out.println("osapo");
-        System.out.println("ujaai");
-        System.out.println("rvlpq");
-        System.out.println("rlsbv");
-        System.out.println("ylpbv");
-        System.out.println("muxsh");
-        System.out.println("dwdwp");
-        System.out.println("wpmtv");
-        System.out.println("cmeae");
-        System.out.println("suqbt");
-        System.out.println("gdjyn");
-        System.out.println("orkad");
-        System.out.println("btxen");
-        System.out.println("lpugw");
+        System.out.println("bbsfq");
+        System.out.println("vwlms");
     }
 }
