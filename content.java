a1ed99b2bf29f3575d17d3f53546702fe6b2798c
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-06-17 13:38:45 -0400

    Feat/C51SMRT-5202 design welcome modal UI (#2405)
    * feat: C51SMRT-5202 initial ui for welcome modal
    
    * feat: C51SMRT-5202 completed the ui using apple's icons
    
    * feat: C51SMRT-5202 replaced placeholder assets with actual assets
    
    * feat: C51SMRT-5202 added usage of welcome modal in OfferListFragment
    
    * feat: C51SMRT-5202 fixed the styles and welcome modal display
    
    * feat: C51SMRT-5202 fixed typos in dialog_instant_offers_modal.xml & OfferListFragment
    
    * feat: C51SMRT-5202 fixed typos again in dialog_instant_offers_modal.xml

--- content.java
+++ content.java
@@ -1,6 +1,11 @@
 public class Cdudsp {
     public static void main() {
         System.out.println("bbsfq");
-        System.out.println("vwlms");
+        System.out.println("cgqls");
+        System.out.println("bjkvk");
+        System.out.println("dvdih");
+        System.out.println("avuim");
+        System.out.println("ilfmh");
+        System.out.println("pkrjm");
     }
 }
