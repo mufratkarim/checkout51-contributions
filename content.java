bb7905bba97ca19898ae48b443c42ab10728cece
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-07-03 15:42:53 -0400

    feat: C51SMRT-5212 remove all the useless code of modals (#2456)

--- content.java
+++ content.java
@@ -3,14 +3,5 @@ public class Cdudsp {
         System.out.println("bbsfq");
         System.out.println("cgqls");
         System.out.println("bjkvk");
-        System.out.println("bfckv");
-        System.out.println("fypwd");
-        System.out.println("eeaaq");
-        System.out.println("qmtax");
-        System.out.println("pdacr");
-        System.out.println("lhuwr");
-        System.out.println("riaah");
-        System.out.println("tvnox");
-        System.out.println("vivis");
     }
 }
