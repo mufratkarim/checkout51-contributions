54aff83ba6c4580307ddd4f3dca64edddb79aa72
Mufrat Aritra <mufratkarimaritra@gmail.com>
2024-08-13 11:33:07 -0400

    feat: C51SMRT-5380 remove base64 image from the app (#2505)
    * feat: C51SMRT-5380 remove base64 and its file
    
    * feat: C51SMRT-5380 remove base64 image on the Redeem page
    
    * feat: C51SMRT-5380 remove all the base64 image in the app
    
    * feat: C51SMRT-5380 headerListItem is only using offer ID, which need to change as well
    
    * feat: C51SMRT-5380 headerListItem is only using offer ID, which need to change as well

--- content.java
+++ content.java
@@ -1,8 +1,26 @@
-public class Cgbcdy {
+public class Cjphid {
     public static void main() {
-        System.out.println("limbg");
-        System.out.println("edljk");
-        System.out.println("balli");
-        System.out.println("vuqac");
+        System.out.println("aqvqu");
+        System.out.println("mtbqo");
+        System.out.println("goxot");
+        System.out.println("jvfmx");
+        System.out.println("rrmmh");
+        System.out.println("blnap");
+        System.out.println("yyaho");
+        System.out.println("jxoic");
+        System.out.println("pvbac");
+        System.out.println("qlaib");
+        System.out.println("wuhro");
+        System.out.println("gjsjx");
+        System.out.println("bkjwg");
+        System.out.println("wluse");
+        System.out.println("kfmfi");
+        System.out.println("ycfji");
+        System.out.println("jyulb");
+        System.out.println("devcc");
+        System.out.println("jxmdi");
+        System.out.println("bngsw");
+        System.out.println("axhkq");
+        System.out.println("gkrnm");
     }
 }
