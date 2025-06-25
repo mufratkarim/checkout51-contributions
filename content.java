7119e30605117c8bdecfb24990b8433bfcc8efb7
Mufrat Aritra <mufratkarimaritra@gmail.com>
2025-06-25 14:28:56 -0400

    feat/C51SMRT-6269: config bs for and15 & and14- devices (#2940)
    * feat/C51SMRT-6269: config bs for and15 & and14- devices
    
    * feat/C51SMRT-6269: config bs for and15 & and14- devices
    
    * feat/C51SMRT-6269: fixing bs for and15 & and14- devices

--- content.java
+++ content.java
@@ -36,6 +36,6 @@ public class Cmwdlf {
         System.out.println("yvnlr");
         System.out.println("jsqow");
         System.out.println("qgfwq");
-        System.out.println("qnefe");
+        System.out.println("fiyjd");
     }
 }
