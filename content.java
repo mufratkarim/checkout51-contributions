7b4581a3f9af95ec76ac7d9d7aad100b639237f0
Mufrat Aritra <mufratkarimaritra@gmail.com>
2025-08-20 15:16:14 -0400

    fix: C51SMRT-6439 the old offerListRepository return null in the BarcodeActivity (#3010)
    * fix: C51SMRT-6439 the old offers repository will return null when first navigate to the barcode activity
    
    * fix: C51SMRT-6439 remove logs
    
    * fix: C51SMRT-6439 revert debug code
    
    * fix: C51SMRT-6439 remove logs

--- content.java
+++ content.java
@@ -1,6 +1,18 @@
-public class Cqopov {
+public class Cmroqd {
     public static void main() {
-        System.out.println("iuatw");
-        System.out.println("bqjsy");
+        System.out.println("fsmoa");
+        System.out.println("dlxij");
+        System.out.println("baaeb");
+        System.out.println("gecye");
+        System.out.println("wgrqd");
+        System.out.println("lpyrv");
+        System.out.println("vpbeq");
+        System.out.println("vefig");
+        System.out.println("okisa");
+        System.out.println("kxehi");
+        System.out.println("rleds");
+        System.out.println("wfqeo");
+        System.out.println("lqfyf");
+        System.out.println("rjhlx");
     }
 }
