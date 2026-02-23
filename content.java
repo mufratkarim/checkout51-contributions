09b36f9974cd757535a1ef4ac634bdfedd168ea8
Mufrat Aritra <mufratkarimaritra@gmail.com>
2026-02-23 11:36:52 -0500

    Feat/c51 smrt 6745 clean getoffers endpoint v2 (#3131)
    * feat: C51SMRT-6745 remove "getOffer" endpoint - part 2
    
    * feat: C51SMRT-6745 remove "getOffer" endpoint - part 2
    
    * feat: C51SMRT-6745 remove "getOffer" endpoint - part 2

--- content.java
+++ content.java
@@ -1,13 +1,4 @@
 public class Cbdvxj {
     public static void main() {
-        System.out.println("llxpy");
-        System.out.println("diswk");
-        System.out.println("tpioy");
-        System.out.println("ehqie");
-        System.out.println("edtsd");
-        System.out.println("yuebm");
-        System.out.println("eqfia");
-        System.out.println("rgaci");
-        System.out.println("pixkq");
     }
 }
